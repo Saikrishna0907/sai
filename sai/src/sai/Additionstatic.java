97bf788e9e7efdc81e9049f10fd66a7ced726b4c
176496 <176496@INHYDDIS5HBFL33.AMERICAS.GLOBAL.NTTDATA.COM>
2020-06-12 13:19:40 +0530

    Added another feature

---
+++ sai/src/sai/Additionstatic.java
@@ -0,0 +1,19 @@
+package sai;
+import java.util.*;
+public class Additionstatic {
+	public static void main (String sai[])
+	{
+		Scanner in = new Scanner(System.in);
+		int a,b,c,d;
+		System.out.println("Enter the a value");
+		a=in.nextInt();
+		//b=3;
+		System.out.println("Enter the b value");
+		b=in.nextInt();
+		c=a+b;
+		d=a-b;
+		System.out.println("Add of Result "+c);
+		System.out.print("Sub of Result "+d);
+	}
+
+}
