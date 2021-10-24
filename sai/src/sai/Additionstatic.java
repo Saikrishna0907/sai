43afa7cf5dccd99725c96aaae497072e95cb9828
Saikrishna Tirupathi <66712442+Saikrishna0907@users.noreply.github.com>
2021-10-24 12:03:43 +0530

    removed unnecessary data

--- sai/src/sai/Additionstatic.java
+++ sai/src/sai/Additionstatic.java
@@ -7,7 +7,6 @@ public class Additionstatic {
 		int a,b,c,d;
 		System.out.println("Enter the a value");
 		a=in.nextInt();
-		//b=3;
 		System.out.println("Enter the b value");
 		b=in.nextInt();
 		c=a+b;
